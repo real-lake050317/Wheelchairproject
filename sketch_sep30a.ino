55ffdcb1578a4c0501c340bde29c77b938f70271
jinojoa317 <jinojoa317@naver.com>
2021-10-23 01:49:26 +0900

    revision on arduino code - ver 0.0.2

--- sketch_sep30a.ino
+++ sketch_sep30a.ino
@@ -10,9 +10,17 @@ void setup() {
   pinMode(sensor, INPUT);
 }
 
+void set_speed(){
+  speed = analogRead(A0)/"""X"""; //아날로그값 리드, 범위에 맞게 속도로 조절
+  analogWrite(leftdc, speed);
+  analogWrite(rightdc, speed);
+
+}
+
 void loop(){
   int control = 0;
   int temp;
+  set_speed();
   if (Serial.available()){
     temp = control;
     control = Serial.read();
@@ -21,17 +29,15 @@ void loop(){
     continue;
   }
   else{
-    //시리얼 통신 -> openCV값 전달, 기존값과 비교 먼저 -> 안하면 모터 깔짝깔짝
+    //시리얼 통신 -> CV 리딩값 전달, 기존값과 비교 먼저 -> 안하면 모터 깔짝깔짝
     int a = analogRead(A0)/"""x""" //아날로그값을 리딩 + 적절하게 범위에 맞게 조정
     digitalWrite(leftdc, a);
     digitalWrite(rightdc, a);
     if (control == "0"){ // 전진 조건 + 속도조건?
       digitalWrite(leftdc, HIGH);
       digitalWrite(rightdc, HIGH);
-      //analogWrite(leftdc, """VARIATION""");//모터속도조건에 따라서 변수 설정
-      //analogWrite(rightdc, """VARIATION""");//모터속도조건에 따라서 변수 설정
     }
-    if (control = "3"){ //오른쪽 회전조건;
+    if (control = "3"){ //오른쪽 회전조건
       digitalWrite(leftdc, HIGH);
       digitalWrite(rightdc, LOW);
     }
@@ -42,8 +48,6 @@ void loop(){
     if (control == "1"){ //후진조건
       digitalWrite(leftdc, LOW);
       digitalWrite(rightdc, LOW);
-      //analogWrite(leftdc, """VARIATION""");//모터속도조건에 따라서 변수 설정
-      //analogWrite(rightdc, """VARIATION""");//모터속도조건에 따라서 변수 설정
     }
   }
 }
