700ba3a93aeb79f8ad68a421d9921ac11f69c088
jinojoa317 <jinojoa317@naver.com>
2021-11-04 22:04:19 +0900

    updated gui program

--- gui.py
+++ gui.py
@@ -53,9 +53,8 @@ b3.place(relwidth=0.5, relheight=0.25, relx=0.05, rely=0.7)
 label_frame4 = tkinter.LabelFrame(window, text="sms", relief="solid", bd=2)
 label_frame4.place(relwidth=0.3, relheight=0.9, relx=0.6, rely=0.05)
 
-
-# 어떤 체크버튼이 눌렸는지 확인하고 cl리스트에 집어넣기
-cl = []
+# 어떤 체크버튼이 눌렸는지 확인하고 cl리스트(전화번호딕셔너리)에 집어넣기
+cl = {}
 def clickme():
     global cl
     str = ''
@@ -64,10 +63,11 @@ def clickme():
         if 'mom' in cl:
             pass
         else:
-            cl.append('mom')
+            cl['mom']='010-0000-0000'
+
     else:
         if 'mom' in cl:
-            cl.remove('mom')
+            del cl['mom']
         else:
             pass
     if CheckVariety_2.get() ==1:
@@ -75,10 +75,10 @@ def clickme():
         if 'dad' in cl:
             pass
         else:
-            cl.append('dad')
+            cl['dad'] = '010-1111-1111'
     else:
         if 'dad' in cl:
-            cl.remove('dad')
+            del cl['dad']
         else:
             pass
     if CheckVariety_3.get() == 1:
@@ -86,10 +86,10 @@ def clickme():
         if 'friend1' in cl:
             pass
         else:
-            cl.append('friend1')
+            cl['friend1'] = '010-2222-2222'
     else:
         if 'friend1' in cl:
-            cl.remove('friend1')
+            del cl['friend1']
         else:
             pass
     if str == '':
@@ -124,10 +124,16 @@ def deselectall():
 buttondeselectall = tkinter.Button(window, text="전체취소", overrelief="solid", command=deselectall)
 buttondeselectall.place(relx=0.61, rely=0.2, relwidth=0.1)
 
-# 리스트 박스 생성
+# 리스트 박스 생성(txt 에 아무것도 없으면 기본, 있으면 그걸로 실행)
+
+with open("listbox.txt") as f:
+    mylist = f.read().splitlines()
+    if mylist == []:
+        messages = ['지금가요', '학교에요', '집이에요', '바빠요']
+    else:
+        messages = mylist
 
 listbox = tkinter.Listbox(window, selectmode='single', height=0)
-messages = ['지금가요', '학교에요', '집이에요', '바빠요']
 for i in range(len(messages)):
     listbox.insert(i, messages[i])
 listbox.place(relx=0.745, rely=0.15, relwidth = 0.1)
@@ -140,6 +146,7 @@ def add(event):
         messages.append(entry.get())
         listbox.insert(len(messages), entry.get())
         entry.delete(0, 'end')
+        print(messages)
     else:
         messagebox.showwarning("경고", "한도를 초과하였습니다")
 
@@ -161,6 +168,7 @@ def delete():
 # 삭제버튼
 buttonDel = tkinter.Button(window, text="선택항목 삭제", overrelief="solid", command=delete)
 buttonDel.place(relx=0.745, rely=0.09, relwidth=0.1, relheight=0.03)
+
 # entry 생성
 entry = tkinter.Entry(window)
 entry.bind("<Return>", add)
@@ -169,11 +177,11 @@ entry.place(relx=0.745, rely=0.117, relwidth=0.1, relheight=0.03)
 #리스트 박스 저장 함수, 버튼
 def save():
     with open('listbox.txt', 'w') as f:
-        f.write(', '.join(listbox.get(0, END)))
+        f.write('\n'.join(listbox.get(0, END)))
         f.close()
 
 buttonsave = tkinter.Button(window, text="save", overrelief="solid", command=save)
-buttonsave.place(relx=0.745, rely = 0.3, relwidth = 0.1, relheight = 0.1)
+buttonsave.place(relx=0.85, rely = 0.15, relwidth = 0.045, relheight = 0.15)
 
 
 window.mainloop()
