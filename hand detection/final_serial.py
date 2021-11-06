158ae469bd328e56e02aa858a90d9424501e714a
realtonypark <34884758+realtonypark@users.noreply.github.com>
2021-11-06 15:45:06 +0900

    시리얼 추가
    시리얼 추가

---
+++ hand detection/final_serial.py
@@ -0,0 +1,120 @@
+import cv2
+import mediapipe as mp
+import time
+import serial
+ser = serial.Serial("3", 9600)
+
+#04:03:20
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 13, (255, 0, 255), cv2.FILLED)
+
+        return lmList
+
+
+def main():
+    #4는 엄지 끝, 8은 검지 끝, 12는 중지 끝...
+    fingertips = [4, 8, 12, 16, 20]
+
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            fingers = []
+
+            #엄지 손가락
+            if lmList[fingertips[0]][1] > lmList[fingertips[0]-1][1]:
+                fingers.append(1)
+                #펴져 있으면 리스트에 1 추가
+            else:
+                fingers.append(0)
+                #접혀 있으면 리스트에 0 추가
+
+            #나머지 손가락
+            for id in range(1,5):
+                if lmList[fingertips[id]][2] < lmList[fingertips[id]-2][2]:
+                    fingers.append(1)
+                    #펴져 있으면 리스트에 1 추가
+                else:
+                    fingers.append(0)
+                    #접혀 있으면 리스트에 0 추가
+            
+            #print(fingers)
+            
+            if fingers == [0,0,0,0,0]: 
+                command = "stop"
+                temp = command.encode("utf-8")
+                ser.write(temp)
+
+            elif fingers == [0,1,0,0,0]: 
+                command = "go straight"
+                temp = command.encode("utf-8")
+                ser.write(temp) 
+
+            elif fingers == [0,1,1,0,0]: 
+                command = "go backward"
+                temp = command.encode("utf-8")
+                ser.write(temp)
+
+            elif fingers == [1,0,0,0,0]: 
+                command = "go left"
+                temp = command.encode("utf-8")
+                ser.write(temp)
+
+            elif fingers == [0,0,0,0,1]: 
+                command = "go right"
+                temp = command.encode("utf-8")
+                ser.write(temp)
+            
+            #totalFingers = fingers.count(1) 펴진 손가락 총 개수 세기(필요 없음)
+            #print(totalFingers)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
