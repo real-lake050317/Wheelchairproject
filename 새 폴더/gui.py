32311fab6c479e1fd7bffb4bd3c7dcd4b8a3cc61
jinojoa317 <jinojoa317@naver.com>
2021-11-06 15:31:59 +0900

    issues for gui program, line 136

--- 새 폴더/gui.py
+++ 새 폴더/gui.py
@@ -1,27 +1,32 @@
 import tkinter
 from tkinter import messagebox
 from tkinter import END
+
 window = tkinter.Tk()
 window.title("wheelchair project")
-window.configure(bg= '#FFFAFA')
+window.configure(bg='#FFFAFA')
 window.attributes('-fullscreen', True)
 
 
-# map에 대한 toplevel 생성함수
+# map 에 대한 toplevel 생성함수
 def createNewWindow1():
     newWindow = tkinter.Toplevel(window)
     labelExample = tkinter.Label(newWindow, text="map")
     labelExample.pack()
     newWindow.attributes('-fullscreen', True)
-    exit_button = tkinter.Button(newWindow, text = "exit", command = newWindow.destroy)
-    exit_button.place(relx = 0.45, rely = 0.95, relwidth = 0.1, relheight = 0.05)
+    exit_button = tkinter.Button(newWindow, text="exit", command=newWindow.destroy)
+    exit_button.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.05)
+
+
 """
-#종료버튼 있는 전체화면 창
+종료버튼 있는 전체화면 창
     width = window.winfo_screenwidth()
     height = window.winfo_screenheight()
     newWindow.geometry("%dx%d" % (width, height))
 """
-# work에 대한 toplevel 생성함수
+
+
+# work 에 대한 toplevel 생성함수
 def createNewWindow2():
     newWindow = tkinter.Toplevel(window)
     labelExample = tkinter.Label(newWindow, text="work")
@@ -31,7 +36,7 @@ def createNewWindow2():
     exit_button.place(relx=0.45, rely=0.95, relwidth=0.1, relheight=0.05)
 
 
-# info에 대한 toplevel 생성함수
+# info 에 대한 toplevel 생성함수
 def createNewWindow3():
     newWindow = tkinter.Toplevel(window)
     labelExample = tkinter.Label(newWindow, text="info")
@@ -55,22 +60,24 @@ label_frame4.place(relwidth=0.3, relheight=0.9, relx=0.6, rely=0.05)
 
 # 어떤 체크버튼이 눌렸는지 확인하고 cl리스트(전화번호딕셔너리)에 집어넣기
 cl = {}
-def clickme():
+
+
+def send_to():
     global cl
     str = ''
-    if CheckVariety_1.get() ==1:
+    if CheckVariety_1.get() == 1:
         str = str + 'mom clicked, '
         if 'mom' in cl:
             pass
         else:
-            cl['mom']='010-0000-0000'
+            cl['mom'] = '010-0000-0000'
 
     else:
         if 'mom' in cl:
             del cl['mom']
         else:
             pass
-    if CheckVariety_2.get() ==1:
+    if CheckVariety_2.get() == 1:
         str = str + 'dad clicked, '
         if 'dad' in cl:
             pass
@@ -97,6 +104,7 @@ def clickme():
     messagebox.showinfo("Button Clicked", str)
     print(cl)
 
+
 # 체크버튼 숫자 get
 CheckVariety_1 = tkinter.IntVar()
 CheckVariety_2 = tkinter.IntVar()
@@ -109,10 +117,10 @@ checkbutton1.place(relx=0.61, rely=0.1, relwidth=0.1)
 checkbutton2.place(relx=0.61, rely=0.13, relwidth=0.1)
 checkbutton3.place(relx=0.61, rely=0.16, relwidth=0.1)
 
-
 # 어떤 체크박스를 클맀했는지 확인
-action = tkinter.Button(window, text="click me", command=clickme)
+action = tkinter.Button(window, text="click me", command=send_to)
 action.place(relx=0.61, rely=0.07, relwidth=0.1)
+
 
 # 전체 체크버튼 취소함수
 def deselectall():
@@ -125,7 +133,11 @@ buttondeselectall = tkinter.Button(window, text="전체취소", overrelief="soli
 buttondeselectall.place(relx=0.61, rely=0.2, relwidth=0.1)
 
 # 리스트 박스 생성(txt 에 아무것도 없으면 기본, 있으면 그걸로 실행)
-
+"""
+with open('listbox.txt', 'w') as f:
+    f.write('')
+    f.close()
+"""
 with open("listbox.txt") as f:
     mylist = f.read().splitlines()
     if mylist == []:
@@ -136,7 +148,7 @@ with open("listbox.txt") as f:
 listbox = tkinter.Listbox(window, selectmode='single', height=0)
 for i in range(len(messages)):
     listbox.insert(i, messages[i])
-listbox.place(relx=0.745, rely=0.15, relwidth = 0.1)
+listbox.place(relx=0.745, rely=0.15, relwidth=0.1)
 
 
 # 리스트 박스 데이터 추가
@@ -150,12 +162,13 @@ def add(event):
     else:
         messagebox.showwarning("경고", "한도를 초과하였습니다")
 
+
 # 리스트 박스 데이터 삭제
 def delete():
     global messages
     if len(messages) > 0:
         selection = listbox.curselection()
-        if (len(selection) == 0):
+        if len(selection) == 0:
             return
         value = listbox.get(selection[0])
         ind = messages.index(value)
@@ -165,23 +178,48 @@ def delete():
     else:
         messagebox.showwarning("경고", "한도를 초과하였습니다")
 
+
 # 삭제버튼
 buttonDel = tkinter.Button(window, text="선택항목 삭제", overrelief="solid", command=delete)
 buttonDel.place(relx=0.745, rely=0.09, relwidth=0.1, relheight=0.03)
 
-# entry 생성
+# 리스트 박스 추가용 entry 생성
 entry = tkinter.Entry(window)
 entry.bind("<Return>", add)
 entry.place(relx=0.745, rely=0.117, relwidth=0.1, relheight=0.03)
 
-#리스트 박스 저장 함수, 버튼
+
+# 리스트 박스 저장 함수, 버튼
+
 def save():
     with open('listbox.txt', 'w') as f:
         f.write('\n'.join(listbox.get(0, END)))
         f.close()
+    messagebox.showinfo("알림", "저징되었습니다")
+
 
 buttonsave = tkinter.Button(window, text="save", overrelief="solid", command=save)
-buttonsave.place(relx=0.85, rely = 0.15, relwidth = 0.045, relheight = 0.15)
+buttonsave.place(relx=0.85, rely=0.09, relwidth=0.045, relheight=0.06)
+
+
+#메세지 send버튼
+
+send = tkinter.Button(window, text="send", overrelief = "solid")
+send.place(relx = 0.7, rely = 0.5, relwidth=0.1)
+
+#메세지 send entry
+
+def set_text():
+    entry2.delete(0, END)
+    for i in listbox.curselection():
+        entry2.insert(0, messages[i])
+
+entry2 = tkinter.Entry(window)
+#entry.bind("<return>", send_messages) --> 메세지 송신 함수를 만든후 추가
+entry2.place(relx=0.625, rely=0.7, relwidth=0.25, relheight=0.2)
+
+buttontext = tkinter.Button(window, text="입력", command = set_text)
+buttontext.place(relx=0.65, rely = 0.9, relwidth = 0.1)
 
 
 window.mainloop()
