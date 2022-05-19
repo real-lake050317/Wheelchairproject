56ee5ef7e328db6fdf67bf3ed993c405a82bd5a1
real-lake050317 <jinojoa317@naver.com>
2022-05-19 23:02:41 +0900

    Work on arduino motor control

--- mechanics/motorcontrol/sketch_nov06b.ino
+++ mechanics/motorcontrol/sketch_nov06b.ino
@@ -1,9 +1,11 @@
-char receivedChar;
-boolean newData = false;
-const int in1 = 11;
-const int in2 = 10;
-const int in3 = 9;
-const int in4 = 6;
+char receivedChar; //Serial 데이터를 수신해서 저장하기 위한 변수
+boolean newData = false; //새로운 Serial 데이터가 수신되었는지 확인하기 위한 변수
+const int in1 = 8; //모터1(왼쪽 모터) 제어를 위한 + 핀
+const int in2 = 7; //모터2(오른쪽 모터) 제어를 위한 - 핀
+const int in3 = 9; //모터2(오른쪽 모터) 제어를 위한 + 핀
+const int in4 = 6; //모터2(오른쪽 모터) 제어를 위한 - 핀
+const int speed1 = 11; //PWM 핀을 통한 모터 속도 제어, 모터드라이버의 ENA 소켓과 연결
+const int speed2 = 10; //PWM 핀을 통한 모터 속도 제어, 모터드라이버의 ENB 소켓과 연결
 
 void setup() {
     Serial.begin(19200);
@@ -11,15 +13,15 @@ void setup() {
     pinMode(in2, OUTPUT);
     pinMode(in3, OUTPUT);
     pinMode(in4, OUTPUT);
-}
+} //모터 제어용 핀들을 OUTPUT 핀으로 설정
 
-void recvOneChar() {
+void recvOneChar() { //새로운 Serial 데이터를 수신하기 위한 함수
     newData = false;
-    if (Serial.available() > 0) {
+    if (Serial.available() > 0) { //Serial.available() 함수는 boolean의 형태로 0, 1로 반환됨
         receivedChar = Serial.read();
         newData = true;
     }
-}
+} 
 
 void motor_one_forward(){
   digitalWrite(in1, HIGH);
@@ -51,37 +53,49 @@ void motor_two_stop(){
   digitalWrite(in4, HIGH);
 }
 
+/* //Deprecation Warning
+void set_speed1(int speed){
+  analogWrite(speed1, speed);
+}
+
+void set_speed2(int speed){
+  analogWrite(speed2, speed);
+}
+*/
+// 모터의 속도 조절을 위한 함수이나 현재 사용되지 않음.
+
+
 void loop() {
-    receivedChar = 'a';
-    recvOneChar();
-    if (newData == true){
-      if (receivedChar == 's'){
-        motor_one_forward();
-        motor_two_forward();
-        delay(10);
-        motor_one_stop();
-        motor_two_stop();
-      }
-      else if (receivedChar == 'b'){
-        motor_one_backward();
-        motor_two_backward();
-        delay(10);
-        motor_one_stop();
-        motor_two_stop();
-      }
-      else if (receivedChar == 'l'){
-        motor_one_backward();
-        motor_two_forward();
-        delay(10);
-        motor_one_stop();
-        motor_two_stop();
-      }
-      else if (receivedChar == 'r'){
-        motor_one_forward();
-        motor_one_backward();
-        delay(10);
-        motor_one_stop();
-        motor_two_stop();
-      }
+  receivedChar = 'a';
+  recvOneChar();
+  if (newData == true){
+    if (receivedChar == 's'){ //Straight, 직진을 하기 위한 Serial이 입력된 경우
+      motor_one_forward();
+      motor_two_forward();
+      delay(10);
+      motor_one_stop();
+      motor_two_stop();
     }
+    else if (receivedChar == 'b'){ //Backwards, 후진을 하기 위한 Serial이 입력된 경우
+      motor_one_backward();
+      motor_two_backward();
+      delay(10);
+      motor_one_stop();
+      motor_two_stop();
+    }
+    else if (receivedChar == 'l'){ //Left, 왼쪽으로 회전을 하기 위한 Serial이 입력된 경우
+      motor_one_backward();
+      motor_two_forward();
+      delay(10);
+      motor_one_stop();
+      motor_two_stop();
+    }
+    else if (receivedChar == 'r'){ //Right, 오른쪽으로 회전을 하기 위한 Serial이 입력된 경우
+      motor_one_forward();
+      motor_one_backward();
+      delay(10);
+      motor_one_stop();
+      motor_two_stop();
+    }
+  }
 }
