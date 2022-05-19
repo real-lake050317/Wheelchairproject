7504f1c8ac277fbdb576e7cd7559c9374b5e467d
Seunghyeon Park <34884758+realtonypark@users.noreply.github.com>
2022-05-20 05:01:33 +0900

    Add files via upload
    mongo update

---
+++ software/tests/demo.py
@@ -0,0 +1,14 @@
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient('mongodb+srv://<user>:<password>@wheelchairproject.qpdsg.mongodb.net/?retryWrites=true&w=majority')
+db = cluster["demo"]
+collection = db["col"]
+
+
+
+post1 = {"lat":50.34, "lang":23.65, "time":"30:40", "date": "2022/05/20"}
+#rem = post1
+post2 = { "$set": {"lat":10.10, "lang":20.20, "time":"30:40", "date": "2022/05/19"} }
+
+collection.update_one(post1, post2)
