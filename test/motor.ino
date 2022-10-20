0b8e070e3f2c4e16387bdec28a784c093cd74486
Jinho Kim <79720217+real-lake050317@users.noreply.github.com>
2022-10-20 10:10:39 +0900

    .

--- test/motor.ino
+++ test/motor.ino
@@ -2,32 +2,34 @@ char receivedChar;
 boolean newData = false;
 
 // Motor A connections
-int enA = 9;
-int in1 = 8;
-int in2 = 7;
+int enA = 11;
+int in1 = 10;
+int in2 = 9;
 // Motor B connections
-int enB = 3;
+int enB = 6;
 int in3 = 5;
 int in4 = 4;
 
 void recvOneChar() { //새로운 Serial 데이터를 수신하기 위한 함수
-    newData = false;
-    if (Serial.available() > 0) { //Serial.available() 함수는 boolean의 형태로 0, 1로 반환됨
-        receivedChar = Serial.read();
-        newData = true;
-    }
+  newData = false;
+  if (Serial.available() > 0) { //Serial.available() 함수는 boolean의 형태로 0, 1로 반환됨
+    receivedChar = Serial.read();
+    newData = true;
+  }
 }
 
 void setup() {
-  analogWrite(enA, 255);
-  analogWrite(enB, 255);
+  Serial.begin(19200);
+
+  analogWrite(enA, 120);
+  analogWrite(enB, 120);
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  
+
   // Turn off motors - Initial state
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
@@ -35,23 +37,59 @@ void setup() {
   digitalWrite(in4, LOW);
 }
 
+void stopMoving() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void goForward() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void goBackward() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void turnLeft() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void turnRight() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
 void loop() {
   receivedChar = 'a';
   recvOneChar();
-  /*
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  delay(1000);
-  
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  delay(1000);
-  
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  delay(1000);
-  */
   if (newData == true) {
-    
+    if (receivedChar == 's') {
+      goForward();
+    }
+    else if (receivedChar == 'b') {
+      goBackward();
+    }
+    else if (receivedChar == 'l') {
+      turnLeft();
+    }
+    else if (receivedChar == 'r') {
+      turnRight();
+    }
+  }
+  else {
+    stopMoving();
   }
 }
